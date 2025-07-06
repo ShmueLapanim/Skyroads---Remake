b21a378811cec1ee035754604b41f132a49333df
Oren Papismedov <orenpapi11@gmail.com>
2025-07-06 19:32:48 +0300

    Added "Refuel" Shader Cartoon Style + Controller Script

---
+++ Assets/Shaders/Platforms/Cartoon/Refuel/RefuelPlatformController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class RefuelPlatformController : MonoBehaviour
+{
+    [SerializeField] private Material platformMaterial;
+    [SerializeField] private bool isPlayerOn = false;
+
+    private float transitionTime = 1f;
+    private float currentFill = 0f;
+    private float targetFill = 0f;
+
+    private void Start()
+    {
+        if (platformMaterial != null && platformMaterial.HasProperty("_LightningTransitionTime"))
+        {
+            transitionTime = platformMaterial.GetFloat("_LightningTransitionTime");
+        }
+    }
+
+    private void Update()
+    {
+        targetFill = isPlayerOn ? 1f : 0f;
+
+        float speed = 1f / Mathf.Max(transitionTime, 0.01f);
+        currentFill = Mathf.MoveTowards(currentFill, targetFill, Time.deltaTime * speed);
+
+        if (platformMaterial != null)
+        {
+            platformMaterial.SetFloat("_IsPlayerOn", currentFill); // שים לב לשם פה!
+        }
+    }
+}
